c95f222e0c7035eeea9b8fbae34b1c080de51365
denydemang <denyk819@gmail.com>
2024-07-07 07:18:19 +0700

    feat (Dump SQL) : Edit users.sql dan upah.sql

--- public/dump_sql/upah.sql
+++ public/dump_sql/upah.sql
@@ -11,31 +11,27 @@
  Target Server Version : 110302 (11.3.2-MariaDB-log)
  File Encoding         : 65001
 
- Date: 30/06/2024 18:01:46
+ Date: 06/07/2024 15:54:32
 */
 
-
--- ----------------------------
--- Table structure for upah
--- ----------------------------
 
 
 -- ----------------------------
 -- Records of upah
 -- ----------------------------
-INSERT INTO `upah` VALUES ('UPAH001', 'Pekerjaan pondasi', 'Pekerjaan pondasi', 'm³', 1500000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:04:39', '2024-06-30 17:55:09');
-INSERT INTO `upah` VALUES ('UPAH002', 'Pekerjaan pemasangan struktur dan rangka bangunan', 'Pekerjaan pemasangan struktur dan rangka bangunan', 'm³', 2700000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:06:06', '2024-06-30 12:03:49');
-INSERT INTO `upah` VALUES ('UPAH003', 'Pekerjaan komponen dinding penutup', 'Pekerjaan komponen dinding penutup', 'm³', 8200000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:06:50', '2024-06-30 12:04:33');
-INSERT INTO `upah` VALUES ('UPAH004', 'Pekerjaan pemasangan kusen', 'Pekerjaan pemasangan kusen', 'pcs', 9000000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:07:52', '2024-06-30 12:05:31');
-INSERT INTO `upah` VALUES ('UPAH005', 'Pekerjaan pemasangan panel pintu', 'Pekerjaan pemasangan panel pintu', 'pcs', 9500000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:08:20', '2024-06-30 12:05:50');
-INSERT INTO `upah` VALUES ('UPAH006', 'Pekerjaan pemasangan jendela', 'Pekerjaan pemasangan jendela', 'pcs', 9000000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:08:47', '2024-06-30 12:06:03');
-INSERT INTO `upah` VALUES ('UPAH007', 'Pekerjaan rangka atap dan penutup atap', 'Pekerjaan rangka atap dan penutup atap', 'm³', 17400000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:09:10', '2024-06-30 12:06:33');
-INSERT INTO `upah` VALUES ('UPAH008', 'Pekerjaan rangka plafond dan penutup plafond', 'Pekerjaan rangka plafond dan penutup plafond', 'm³', 9200000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:09:32', '2024-06-30 12:06:51');
-INSERT INTO `upah` VALUES ('UPAH009', 'Pekerjaan penutup lantai', 'Pekerjaan penutup lantai', 'm³', 8400000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:10:59', '2024-06-30 12:07:08');
-INSERT INTO `upah` VALUES ('UPAH010', 'Pekerjaan plester aci dan pengecatan', 'Pekerjaan plester aci dan pengecatan', 'm³', 18000000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:11:19', '2024-06-30 12:07:30');
-INSERT INTO `upah` VALUES ('UPAH011', 'Pekerjaan instalasi listrik', 'Pekerjaan instalasi listrik', 'ttk/pcs', 3200000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:11:47', '2024-06-30 12:07:53');
-INSERT INTO `upah` VALUES ('UPAH012', 'Pekerjaan plumbing', 'Pekerjaan plumbing', 'pcs', 4900000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:12:07', '2024-06-30 12:08:12');
-INSERT INTO `upah` VALUES ('UPAH013', 'Pekerjaan utilitas rumah', 'Pekerjaan utilitas rumah', 'm³', 2000000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:12:26', '2024-06-30 12:08:40');
+INSERT INTO `upah` VALUES ('UPAH001', 'Pekerjaan pondasi', 'Pekerjaan pondasi', 'm³', 750000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:04:39', '2024-07-06 14:16:36');
+INSERT INTO `upah` VALUES ('UPAH002', 'Pekerjaan pemasangan struktur dan rangka bangunan', 'Pekerjaan pemasangan struktur dan rangka bangunan', 'm³', 540000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:06:06', '2024-07-06 14:17:13');
+INSERT INTO `upah` VALUES ('UPAH003', 'Pekerjaan komponen dinding penutup', 'Pekerjaan komponen dinding penutup', 'm³', 2050000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:06:50', '2024-07-06 14:17:41');
+INSERT INTO `upah` VALUES ('UPAH004', 'Pekerjaan pemasangan kusen', 'Pekerjaan pemasangan kusen', 'pcs', 1800000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:07:52', '2024-07-06 14:19:59');
+INSERT INTO `upah` VALUES ('UPAH005', 'Pekerjaan pemasangan panel pintu', 'Pekerjaan pemasangan panel pintu', 'pcs', 3166667.0000, '50.02', 'admin', 'admin', '2024-06-28 20:08:20', '2024-07-06 14:19:47');
+INSERT INTO `upah` VALUES ('UPAH006', 'Pekerjaan pemasangan jendela', 'Pekerjaan pemasangan jendela', 'pcs', 4500000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:08:47', '2024-07-06 14:19:28');
+INSERT INTO `upah` VALUES ('UPAH007', 'Pekerjaan rangka atap dan penutup atap', 'Pekerjaan rangka atap dan penutup atap', 'm³', 1740000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:09:10', '2024-07-06 14:20:32');
+INSERT INTO `upah` VALUES ('UPAH008', 'Pekerjaan rangka plafond dan penutup plafond', 'Pekerjaan rangka plafond dan penutup plafond', 'm³', 2300000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:09:32', '2024-07-06 14:20:59');
+INSERT INTO `upah` VALUES ('UPAH009', 'Pekerjaan penutup lantai', 'Pekerjaan penutup lantai', 'm³', 1400000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:10:59', '2024-07-06 14:33:32');
+INSERT INTO `upah` VALUES ('UPAH010', 'Pekerjaan plester aci dan pengecatan', 'Pekerjaan plester aci dan pengecatan', 'm³', 3600000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:11:19', '2024-07-06 14:34:02');
+INSERT INTO `upah` VALUES ('UPAH011', 'Pekerjaan instalasi listrik', 'Pekerjaan instalasi listrik', 'ttk/pcs', 640000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:11:47', '2024-07-06 14:39:58');
+INSERT INTO `upah` VALUES ('UPAH012', 'Pekerjaan plumbing', 'Pekerjaan plumbing', 'pcs', 816667.0000, '50.02', 'admin', 'admin', '2024-06-28 20:12:07', '2024-07-06 14:40:34');
+INSERT INTO `upah` VALUES ('UPAH013', 'Pekerjaan utilitas rumah', 'Pekerjaan utilitas rumah', 'm³', 1000000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:12:26', '2024-07-06 14:41:22');
 INSERT INTO `upah` VALUES ('UPAH014', 'Pekerjaan sanitasi air', 'Pekerjaan sanitasi air', 'm³', 1000000.0000, '50.02', 'admin', 'admin', '2024-06-28 20:14:37', '2024-06-30 12:10:26');
 
 SET FOREIGN_KEY_CHECKS = 1;
--- public/dump_sql/users.sql
+++ public/dump_sql/users.sql
@@ -1,27 +1,26 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mariadblocal
+ Source Server         : skripsi
  Source Server Type    : MariaDB
- Source Server Version : 110300 (11.3.0-MariaDB-log)
+ Source Server Version : 110302 (11.3.2-MariaDB-log)
  Source Host           : localhost:3306
- Source Schema         : skripsiptgmc2
+ Source Schema         : testgmj
 
  Target Server Type    : MariaDB
- Target Server Version : 110300 (11.3.0-MariaDB-log)
+ Target Server Version : 110302 (11.3.2-MariaDB-log)
  File Encoding         : 65001
 
- Date: 24/06/2024 14:04:56
+ Date: 06/07/2024 15:55:13
 */
-
 
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('admin', 'kiana', '$2y$12$pkcRlvCdIfmFUQ8YzkgqZegG0AIzX0GiF3af7arLaSUG2xM8eFb62', NULL, 1, 1, NULL, NULL, NULL, NULL);
-INSERT INTO `users` VALUES ('direktur', 'farah', '$2y$12$4sbE8zDDV5tGHhkjAMoDw.9TqP9oLInshSckbZWzKWtHnpKitM6bS', NULL, 4, 1, 'admin', 'admin', '2024-06-23 22:01:33', '2024-06-23 22:01:33');
-INSERT INTO `users` VALUES ('keuangan', 'farah', '$2y$12$OLWYwIZ01w6WKABFZ3lyWeeuV43FEzUVXcLDXrMBm01CzwIUkB0IO', NULL, 2, 1, 'admin', 'admin', '2024-06-23 22:00:41', '2024-06-23 22:00:41');
-INSERT INTO `users` VALUES ('logistik', 'kiana', '$2y$12$2yhS6sScW2vW5Vi4y1E9aOIjgpk9lsPO8CZq27MLQh5JQG7jI8bnq', NULL, 3, 1, 'admin', 'admin', '2024-06-23 22:01:09', '2024-06-23 22:01:09');
+INSERT INTO `users` VALUES ('rizqianaadmin', 'Rizqiana', '$2y$12$pkcRlvCdIfmFUQ8YzkgqZegG0AIzX0GiF3af7arLaSUG2xM8eFb62', NULL, 1, 1, 'admin', 'admin','2024-06-23 22:01:33', '2024-06-23 22:01:33');
+INSERT INTO `users` VALUES ('farahdirektur', 'Farah', '$2y$12$4sbE8zDDV5tGHhkjAMoDw.9TqP9oLInshSckbZWzKWtHnpKitM6bS', NULL, 4, 1, 'admin', 'admin', '2024-06-23 22:01:33', '2024-06-23 22:01:33');
+INSERT INTO `users` VALUES ('ulyakeuangan', 'Ulya', '$2y$12$OLWYwIZ01w6WKABFZ3lyWeeuV43FEzUVXcLDXrMBm01CzwIUkB0IO', NULL, 2, 1, 'admin', 'admin', '2024-06-23 22:00:41', '2024-06-23 22:00:41');
+INSERT INTO `users` VALUES ('zulaikahlogistik', 'Zulaikah', '$2y$12$2yhS6sScW2vW5Vi4y1E9aOIjgpk9lsPO8CZq27MLQh5JQG7jI8bnq', NULL, 3, 1, 'admin', 'admin', '2024-06-23 22:01:09', '2024-06-23 22:01:09');
 
 SET FOREIGN_KEY_CHECKS = 1;
